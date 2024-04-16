6ee1a42b5fb00c987a1ef33399241aeec13ec8ac
NarjisseAjlil <narjisse.ajlil@gmail.com>
2024-04-16 22:46:25 +0200

    feat(home) : add api connection and home page template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,35 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import { Icon } from "@iconify/vue";
+
+const data = ref([]);
+
+onMounted(async () => {
+  const response = await fetch("http://localhost:3000/v1/trips");
+  data.value = await response.json();
+});
+</script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="title">
+    Planificateur <br />
+    de voyages
+  </h1>
+  <p class="subtitle">généré par IA</p>
+
+  <div class="input-container">
+    <input type="text" class="input" />
+  </div>
+
+  <div class="button-container">
+    <button class="start-button">C'est parti !</button>
+    <button class="example-button">Voir un exemple</button>
+  </div>
+
+  <p class="last-trips">Derniers itinéraires</p>
+  <div class="trips">
+    <p class="trip" v-for="item in data">
+      {{ item.prompt }}
+    </p>
+  </div>
 </template>
