286ef3b7a606ce4e488c880e487dd3c8c7919261
NarjisseAjlil <narjisse.ajlil@gmail.com>
2024-04-18 10:11:42 +0200

    fix(app) : fix router

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HomeView from "./views/HomeView.vue";
-import AboutView from "./views/AboutView.vue";
 </script>
 
 <template>
-  <HomeView />
-  <AboutView />
+  <RouterView />
 </template>
