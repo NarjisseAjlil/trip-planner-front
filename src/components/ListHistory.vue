117567c6b735b21c0bee28af5f7f7801d6beb42c
NarjisseAjlil <narjisse.ajlil@gmail.com>
2024-04-19 01:33:50 +0200

    feat(listHistory) : add comment style and import icons

--- src/components/ListHistory.vue
+++ src/components/ListHistory.vue
@@ -1,10 +1,14 @@
+<!-- src/components/ListHistory.vue -->
+
 <script setup>
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
+import { ChevronRight } from "lucide-vue-next";
 
 const data = ref([]);
 const router = useRouter();
 
+// call api
 onMounted(async () => {
   const response = await fetch("http://localhost:3000/v1/trips");
   data.value = await response.json();
@@ -16,10 +20,13 @@ const goToTrip = (id) => {
 </script>
 
 <template>
-  <p class="last-trips">Derniers itinéraires</p>
+  <p class="last-trips" style="margin: 5%; padding-top: 5%">
+    Derniers itinéraires
+  </p>
   <div class="trips">
     <p class="trip" v-for="item in data" @click="goToTrip(item.id)">
       {{ item.prompt }}
+      <ChevronRight />
     </p>
   </div>
 </template>
