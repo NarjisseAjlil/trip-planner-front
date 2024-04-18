87f728d62aea699caaa9436627b4d8d3b70691e7
NarjisseAjlil <narjisse.ajlil@gmail.com>
2024-04-18 10:51:37 +0200

    fix(homeview) : fix template and script

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,9 @@
 <script setup>
-import { ref, onMounted } from "vue";
-import { Icon } from "@iconify/vue";
-
-const data = ref([]);
-
-onMounted(async () => {
-  const response = await fetch("http://localhost:3000/v1/trips");
-  data.value = await response.json();
-});
+import Prompt from "@/components/Prompt.vue";
+import History from "@/components/History.vue";
 </script>
 
 <template>
-  <h1 class="title">
-    Planificateur <br />
-    de voyages
-  </h1>
-  <p class="subtitle">généré par IA</p>
-
-  <div class="input-container">
-    <input type="text" class="input" />
-  </div>
-
-  <div class="button-container">
-    <button class="start-button">C'est parti !</button>
-    <button class="example-button">Voir un exemple</button>
-  </div>
-
-  <p class="last-trips">Derniers itinéraires</p>
-  <div class="trips">
-    <p class="trip" v-for="item in data">
-      {{ item.prompt }}
-    </p>
-  </div>
+  <Prompt></Prompt>
+  <History></History>
 </template>
