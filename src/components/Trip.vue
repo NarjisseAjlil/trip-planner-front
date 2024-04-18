b16f680545976bb9610fe7589e6fbfa8af4bb1f7
NarjisseAjlil <narjisse.ajlil@gmail.com>
2024-04-18 10:50:36 +0200

    feat(trip) : create trip component

---
+++ src/components/Trip.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { Icon } from "@iconify/vue";
+
+const data = ref([]);
+
+onMounted(async () => {
+  const response = await fetch("http://localhost:3000/v1/trips");
+  data.value = await response.json();
+});
+</script>
+
+<template>
+  <p class="last-trips">Derniers itinéraires</p>
+  <div class="trips">
+    <p class="trip" v-for="item in data">
+      {{ item.output }}
+    </p>
+  </div>
+</template>
