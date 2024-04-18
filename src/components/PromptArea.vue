5bb255605bd510adfc328a2e0e30838f8f2576de
NarjisseAjlil <narjisse.ajlil@gmail.com>
2024-04-19 01:35:54 +0200

    feat(promptArea) : add loader

--- src/components/PromptArea.vue
+++ src/components/PromptArea.vue
@@ -5,6 +5,7 @@ import { useRouter } from "vue-router";
 const data = ref([]);
 const prompt = ref();
 const router = useRouter();
+const isLoading = ref(false);
 
 async function sendData() {
   const response = await fetch("http://localhost:3000/v1/trips", {
@@ -20,18 +21,21 @@ async function sendData() {
 }
 
 const sendSubmit = async (event) => {
+  isLoading.value = true;
   const id = await sendData();
   router.push(`trips/${id}`);
+  isLoading.value = false;
 };
 </script>
 
 <template>
+  <i class="bi bi-compass"></i>
   <h1 class="title">
     Planificateur <br />
     de voyages
   </h1>
   <p class="subtitle">généré par IA</p>
-  <form @submit.prevent="sendSubmit">
+  <form @submit.prevent="sendSubmit" v-if="!isLoading">
     <div class="input-container">
       <input type="text" class="input" v-model="prompt" />
     </div>
@@ -42,4 +46,15 @@ const sendSubmit = async (event) => {
       <button class="example-button">Voir un exemple</button>
     </div>
   </form>
+
+  <!-- Loader -->
+
+  <div class="ui-abstergo" v-else="isLoading">
+    <div class="abstergo-loader">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="ui-text">Création de votre voyage</div>
+  </div>
 </template>
