e916c1879aa90e977707ce09642b8f4d14ccd9a8
NarjisseAjlil <narjisse.ajlil@gmail.com>
2024-04-18 17:01:44 +0200

    fix(index) : fix path

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/trips",
+      path: "/trips/:id",
       name: "trips",
       component: TripsView,
     },
