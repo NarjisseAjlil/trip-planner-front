46f882bb1799c0d339e4f10945b85829e0700768
NarjisseAjlil <narjisse.ajlil@gmail.com>
2024-04-19 23:46:23 +0200

    fix(map) : fix map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -6,45 +6,32 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
+import { useRoute } from "vue-router";
+const data = ref();
+const mapLocations = ref([]);
+const map = ref();
 
-const prompt = ref("");
-const key = "MAR8E5drMBTYVXle4Ohm";
+onMounted(async () => {
+  const route = useRoute();
+  const id = route.params.id;
+  map.value = L.map("map").setView([51.505, -0.09], 13);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  }).addTo(map.value);
+  const response = await fetch(`http://localhost:3000/v1/trips/${id}`);
+  const jsonAnswer = await response.json();
 
-onMounted(() => {
-  const map = L.map("map", {
-    center: L.latLng(0, 0),
-    zoom: 5,
+  data.value = jsonAnswer;
+  mapLocations.value = jsonAnswer.output;
+  const markers = mapLocations.value.map((mapPoint) => {
+    return L.marker([
+      mapPoint.location.latitude,
+      mapPoint.location.longitude,
+    ]).addTo(map.value);
   });
-
-  L.tileLayer(
-    `https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}`,
-    {
-      tileSize: 512,
-      zoomOffset: -1,
-      minZoom: 1,
-      attribution:
-        '\u003ca href="https://www.maptiler.com/copyright/" target="_blank"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href="https://www.openstreetmap.org/copyright" target="_blank"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e',
-      crossOrigin: true,
-    }
-  ).addTo(map);
-
-  const updateMap = async () => {
-    const locations = [
-      { lat: 35.6895, lng: 139.6917, name: "Tokyo" },
-      { lat: 34.6937, lng: 135.5023, name: "Kyoto" },
-    ];
-    const bounds = L.latLngBounds();
-    locations.forEach((location) => {
-      const marker = L.marker([location.lat, location.lng]).addTo(map);
-      marker.bindPopup(location.name);
-      bounds.extend([location.lat, location.lng]);
-    });
-    map.fitBounds(bounds);
-  };
-
-  updateMap();
+  const group = L.featureGroup(markers);
+  map.value.fitBounds(group.getBounds());
 });
 </script>
 
